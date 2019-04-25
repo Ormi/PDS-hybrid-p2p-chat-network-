3863186fc60cd6e985ae34cdf7deef3c60038303
miormos <mi.ormos@gmail.com>
2019-04-25 17:32:59 +0200

    arguments for rpc

--- pds18-rpc.py
+++ pds18-rpc.py
@@ -9,33 +9,72 @@
 
 import argparse
 
+def defineStep(args):
+    print(args)
+    if args.peer:
+        if args.command == "message":
+            print("peer message")
+        elif args.command == "getlist":
+            print("peer getlist")            
+        elif args.command == "peers":
+            print("peer peers")            
+        elif args.command == "reconnect":
+            print("peer reconnect")   
+        else:
+            print("Unknow command for Peer")         
+    elif args.node:
+        if args.command == "database":
+            print("node database")   
+        elif args.command == "neighbors":
+            print("node neighbors")                          
+        elif args.command == "connect":
+            print("node connect")     
+        elif args.command == "disconnect":
+            print("node disconnect")     
+        elif args.command == "sync":
+            print("node sync")                                         
+        else:
+            print("Unknow command for Node")         
+    else:
+        print("Error, unknow arguments")
+
 parser = argparse.ArgumentParser(description="Hybrid p2p chat application RPC module")
 group = parser.add_mutually_exclusive_group()
-command_group = parser.add_mutually_exclusive_group()
-parser.add_argument("--id", type=int, required=True, help="--id us unique identifier of peer or node, which RPC must send command")
-group.add_argument("--peer", help="Command for instance of peer")
-group.add_argument("--node", help="Command for instance of server")
+# command_group = parser.add_mutually_exclusive_group()
+group.add_argument("--peer", action='store_true',  help="Command for instance of peer")
+group.add_argument("--node", action='store_true', help="Command for instance of server")
 
-command_group.add_argument("message", help="Command for instance of server")
-parser.add_argument("--from", required="message", help="Command for instance of server")
-parser.add_argument("--to", required="message", help="Command for instance of server")
-parser.add_argument("--message", required="message", help="Command for instance of server")
+parser.add_argument("--command",  help="Command for instance of server")
 
-command_group.add_argument("--getlist", help="Command for instance of server")
-command_group.add_argument("--peers", help="Command for instance of server")
-command_group.add_argument("--reconnect", help="Command for instance of server")
-command_group.add_argument("--database", help="Command for instance of server")
-command_group.add_argument("--neighbors", help="Command for instance of server")
-command_group.add_argument("--connect", help="Command for instance of server")
-command_group.add_argument("--discover", help="Command for instance of server")
-command_group.add_argument("--sync", help="Command for instance of server")
+parser.add_argument("--from", help="Command for instance of server")
+parser.add_argument("--to", help="Command for instance of server")
+parser.add_argument("--message", help="Command for instance of server")
 
-parser.add_argument("--command", required=True, help="with list of parameter specifies commands and parameters related to given RCP call")        
+parser.add_argument("--reg-ipv4", help="Command for instance of server")
+parser.add_argument("--reg-port", help="Command for instance of server")
+# command_group.add_argument("getlist", help="Command for instance of server")
+# command_group.add_argument("peers", help="Command for instance of server")
+# command_group.add_argument("reconnect", help="Command for instance of server")
+# command_group.add_argument("database", help="Command for instance of server")
+# command_group.add_argument("neighbors", help="Command for instance of server")
+# command_group.add_argument("connect", help="Command for instance of server")
+# command_group.add_argument("discover", help="Command for instance of server")
+# command_group.add_argument("sync", help="Command for instance of server")
+
+
 args = parser.parse_args()
 print(args)
 
-# ./pds18-rpc --id <identifikátor> <"--peer"|"--node"> --command <příkaz> --<parametr1> <hodnota_parametru1> ...
+defineStep(args)
 
+# --peer --command message --from <username1> --to <username2> --message <zpráva>
+
+"""
+
+
+
+
+# ./pds18-rpc --id <identifikátor> <"--peer"|"--node"> --command <příkaz> --<parametr1> <hodnota_parametru1> ...
 # --peer --command message --from <username1> --to <username2> --message <zpráva>, který se pokusí odeslat chat zprávu
 # --peer --command getlist, který vynutí aktualizaci seznamu v síti známých peerů, tj. odešle zprávu GETLIST a nechá si ji potvrdit
 # --peer --command peers, který zobrazí aktuální seznam peerů v síti, tj. peer si s node vymění zprávy GETLIST a LIST, přičemž obsah zprávy LIST vypíše
@@ -50,3 +89,53 @@ print(args)
 
 
 # python3 pds18-node.py --id 1 --reg-ipv4 127.0.0.1 --reg-port 3000
+
+# import socket
+# import sys
+# import time
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# server_address = ('localhost', 9999)
+
+# message = b'GETLIST'
+
+# try:
+
+#     # Send data
+#     print('sending {!r}'.format(message))
+#     sent = sock.sendto(message, server_address)
+
+#     # Receive response
+#     # print('waiting to receive')
+#     # data, server = sock.recvfrom(4096)
+#     # print('received {!r}'.format(data))
+
+# finally:
+#     print('closing socket')
+#     sock.close()
+
+
+# time.sleep(10)
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# server_address = ('127.0.0.1', 10999)
+
+# message = b'MESSAGE'
+
+# try:
+
+#     # Send data
+#     print('sending {!r}'.format(message))
+#     sent = sock.sendto(message, server_address)
+
+#     # Receive response
+#     # print('waiting to receive')
+#     # data, server = sock.recvfrom(4096)
+#     # print('received {!r}'.format(data))
+
+# finally:
+#     print('closing socket')
+#     sock.close()
+"""
